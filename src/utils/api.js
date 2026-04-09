892641471f3f137f962cfcd9ce316e5595b7a1a1
WajahatQadri <muhammadwajahathussainchishti@gmail.com>
2026-04-09 15:51:31 +0500

    Fix: third party cookies error

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,9 +3,10 @@ import axios from "axios";
 const api = axios.create({
     baseURL: window.location.hostname === "localhost" 
     ? "http://localhost:5000/api/v1"  // Your local port
-    : "https://backend-hospital-project-task-smit.vercel.app/api/v1",
+    : "/api/v1",
     withCredentials : true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+    // methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 });
+
 
 export default api;
